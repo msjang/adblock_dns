e36de5bf1d0832db57abd98f44b75518f7972d47
MinSeok Jang <shuggie@shuggie-mbp.local>
2015-02-24 03:09:40 +0900

    Initial get_dnsmasq_conf program upload

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # get_dnsmasq_conf
 create dnsmasq.conf from domain list to block advertisement
+
+## how to use
+1. Prepare ad_domain_list.txt
+```
+ad.about.co.kr
+ad4980.kr
+ad4989.co.kr
+ad9.kr
+advs.hankyung.com
+adcrm.co.kr
+adtechus.com
+atdmt.com
+...
+```
+2. run the following command
+```
+usage:   ./get_dnsmasq_conf.py <IP> <ad_domain_list>
+example: ./get_dnsmasq_conf 192.168.0.8 ad_domain_list.txt
+```
+3. then, you will get the content of dnsmasq.conf to block advertisement
+```
+address=/.ad.about.co.kr/192.168.0.8    #about
+address=/.img.ad-indicator.com/192.168.0.8  #ad-indicator
+address=/.tag.ad-indicator.com/192.168.0.8  #ad-indicator
+address=/.ad134m.com/192.168.0.8    #ad134m
+address=/.ad4980.kr/192.168.0.8 #ad4980
+address=/.ad4989.co.kr/192.168.0.8  #ad4989
+address=/.ad9.kr/192.168.0.8    #ad9
+...
+```
---
+++ get_dnsmasq_conf.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+import sys, re, fileinput
+
+if len(sys.argv) is not 3:
+	print "usage:   ./get_dnsmasq_conf.py <IP> <ad_domain_list>"
+	print "example: ./get_dnsmasq_conf 192.168.0.8 ad_domain_list.txt"
+	exit()
+
+IP = sys.argv[1]
+
+urls = dict()
+for line in fileinput.input(sys.argv[2]):
+	#remove newline
+	line = line.replace("\r","").replace("\n","")
+	#remove general subdomain
+	#line = line.replace("www.","")
+	line = re.sub("^www\.", "", line)
+	#change top domain
+	line = line.replace("co.kr","co_kr")
+	#get domain name
+	parse = [x for x in line.split('.')]
+	name = parse[-2]
+	#revert top domain
+	line = line.replace("co_kr","co.kr")
+	#insert url to dict without duplicates
+	if name in urls:
+		if line not in urls[name]:
+			urls[name].append(line)
+	else:
+		urls[name]=[]
+		urls[name].append(line)
+
+for name in sorted(urls.keys()):
+	#get urls_to_exclude via exclude sub domain
+	#urls[name] = [y[::-1] for y in sorted([x[::-1] for x in urls[name]])]
+	urls_to_exclude = []
+	#print ""
+	#print name
+	#print "urls:\t",
+	#print urls[name]
+	for url in urls[name]:
+		u = [s for s in urls[name] if url in s][1:]
+		urls_to_exclude = urls_to_exclude + u
+		#print "sub[%s]:\t" % url,
+		#print u
+	urls[name] = [url for url in urls[name] if url not in urls_to_exclude]
+	#generate dnsmasq conf file
+	for url in sorted(urls[name]):
+		#print url
+		print "address=/.%s/%s\t#%s" % (url, IP, name)
