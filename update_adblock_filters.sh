8e26bc9e9456336ed53b42fffca118eaa3c1a2f2
MinSeok Jang <shuggie@shuggie-mbp.local>
2015-03-10 02:10:16 +0900

    repo name changed to adblock_dns

---
+++ update_adblock_filters.sh
@@ -0,0 +1,7 @@
+# adblock filter for English
+# https://easylist.adblockplus.org/en/
+wget -O easylist.txt https://easylist-downloads.adblockplus.org/easylist.txt
+# adblock filter for Korean
+# https://groups.google.com/forum/#!forum/ad-punisher
+wget -O ad_punisher_abp.txt https://groups.google.com/group/ad-punisher/attach/b3bd9181002fd004/Ad-Punisher_Abp.txt?part=0.1&authuser=0&view=1
+echo "adblock lists updated"
